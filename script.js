fe5effc01099ee2aa349988bd97ff2df4c3b61a9
MGCrafter <xjessyx@nekozdevteam.eu>
2024-05-30 16:49:10 +0200

    Change

--- script.js
+++ script.js
@@ -18,40 +18,4 @@ document.addEventListener('DOMContentLoaded', () => {
         animateRainbowBorder(button);
     });
     animateRainbowBorder(footer);
-
-    function adjustElements() {
-        const screenWidth = window.innerWidth;
-
-        linkButtons.forEach(button => {
-            const buttonWidth = screenWidth < 768 ? '95%' : '80%';
-            button.style.width = buttonWidth;
-        });
-    }
-
-    adjustElements();
-    window.addEventListener('resize', adjustElements);
-
-    // Kommentar für Neigungs-Hover Effekt
-    /*
-    // Funktion, um die stärkere Neigung der Buttons bei Mausbewegung zu steuern
-    linkButtons.forEach(button => {
-        button.addEventListener('mousemove', (e) => {
-            const rect = button.getBoundingClientRect();
-            const mouseX = e.clientX - rect.left;
-            const mouseY = e.clientY - rect.top;
-
-            const centerX = rect.width / 2;
-            const centerY = rect.height / 2;
-
-            const deltaX = (mouseX - centerX) / centerX;
-            const deltaY = (mouseY - centerY) / centerY;
-
-            button.style.transform = `rotateX(${deltaY * 30}deg) rotateY(${deltaX * 30}deg)`;
-        });
-
-        button.addEventListener('mouseleave', () => {
-            button.style.transform = '';
-        });
-    });
-    */
 });
